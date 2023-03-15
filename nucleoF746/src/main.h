9e324d8e92972700cfabb084e343389f4a5a175b
DamianPodrazaCode <damian.podraza@gmail.com>
2023-03-15 21:32:02 +0100

    modified:   nucleoF746/platformio.ini

---
+++ nucleoF746/src/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "stm32f7xx_hal.h"
+#include "clocks.h"
+#include "error_handler.h"
+
+#include "led/ledConfig.h"
+//#include "stm32f2xx_nucleo_144.h"
+
+#endif // MAIN_H
